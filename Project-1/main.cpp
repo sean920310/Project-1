c136d6b3227d0f21314e59bd36385d082d3cf96c
承甲 羅 <zaxzyx@gmail.com>
2022-04-20 14:51:29 +0800

    Number fix iszero()

--- Project-1/main.cpp
+++ Project-1/main.cpp
@@ -7,8 +7,8 @@ using namespace std;
 
 int main()
 {
-	string input = "1001";
-	Number num1 = "1000", num2(input);
+	string input = "0.1";
+	Number num1 = "1.1", num2(input);
 	try
 	{
 		cout << "num1: " << num1 << endl;
@@ -23,5 +23,30 @@ int main()
 	{
 		cout << c;
 	}
+
+	/*
+	string input = "0.1";
+		Number num1 = "10", num2(input);
+
+		cout << "+: " << num1 + num2 << endl;
+
+		string input = "9.9";
+		Number num1 = "0.1", num2(input);
+
+		cout << "+: " << num1 + num2 << endl;
+
+		string input = "0.9";
+		Number num1 = "0.1", num2(input);
+
+		cout << "+: " << num1 + num2 << endl;
+
+		string input = "2";
+		Number num1 = "99", num2(input);
+		cout << "+: " << num1 + num2 << endl;
+
+		string input = "10.1";
+		Number num1 = "10.1", num2(input);
+		cout << "+: " << num1 + num2 << endl;
+		*/
 
 }
